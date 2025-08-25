d89a06e49e1ba3a123ac28c54b169ba5e93eb2c5
aditya gunawan saputra <aditya.gunawan1994@gmail.com>
2025-08-25 13:35:58 +0700

    ini

---
+++ src/test/java/com/vibe_coding/vibe_coding/VibeCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vibe_coding.vibe_coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
